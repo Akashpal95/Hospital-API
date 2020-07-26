832669f288c55e4647109a352eee928a5cddc414
Akashpal95 <akash.pal95@gmail.com>
2020-07-26 20:11:50 +0530

    Add dev and test config for database

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ const port = 8000; //port 80 for production level code
 const expressLayouts = require('express-ejs-layouts')
 const session = require('express-session');
 const passport = require('passport');
-
 //Although these are not directly being used here, it needs to loaded here to work
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
 
-const db = require('./config/mongoose');
+const db = require(`./config/${process.env.NODE_ENV}`);
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
-
 //The order of these middlewares are very important
 //mongo store is used to store the session in the db rather in the local
 app.use(session({
